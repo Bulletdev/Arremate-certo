0fdaced00f7437bf9c552826399da0291b72d2ad
Michael Bullet <craquebullet@gmail.com>
2025-03-05 16:26:43 -0300

    Update LicitacaoAppTest.java

--- src/test/java/com/bulletinfotech/licitacao/LicitacaoAppTest.java
+++ src/test/java/com/bulletinfotech/licitacao/LicitacaoAppTest.java
@@ -1,13 +1,56 @@
-
 package com.bulletinfotech.licitacao;
 
 import org.junit.jupiter.api.Test;
+import com.bulletinfotech.licitacao.model.Pregao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LicitacaoAppTest {
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void pregaoConstructorShouldInitializeFieldsCorrectly() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        assertEquals("Description", pregao.getDescricao());
+        assertEquals("Open", pregao.getSituacao());
+        assertEquals(1000.0, pregao.getValorProposto());
+    }
+
+    @Test
+    public void getDescricaoShouldReturnCorrectDescription() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        assertEquals("Description", pregao.getDescricao());
+    }
+
+    @Test
+    public void getSituacaoShouldReturnCorrectSituacao() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        assertEquals("Open", pregao.getSituacao());
+    }
+
+    @Test
+    public void getValorPropostoShouldReturnCorrectValorProposto() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        assertEquals(1000.0, pregao.getValorProposto());
+    }
+
+    @Test
+    public void setDescricaoShouldUpdateDescricao() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        pregao.setDescricao("New Description");
+        assertEquals("New Description", pregao.getDescricao());
+    }
+
+    @Test
+    public void setSituacaoShouldUpdateSituacao() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        pregao.setSituacao("Closed");
+        assertEquals("Closed", pregao.getSituacao());
+    }
+
+    @Test
+    public void setValorPropostoShouldUpdateValorProposto() {
+        Pregao pregao = new Pregao("Description", "Open", 1000.0);
+        pregao.setValorProposto(2000.0);
+        assertEquals(2000.0, pregao.getValorProposto());
     }
 }
