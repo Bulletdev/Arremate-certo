2ca67a35610bd17d07d2f52a95371231a4d3c6f4
Michael Bullet <contato@michaelbullet.com>
2025-03-05 17:05:27 -0300

    Update ApiService.java

--- src/main/java/com/bulletinfotech/licitacao/service/ApiService.java
+++ src/main/java/com/bulletinfotech/licitacao/service/ApiService.java
@@ -6,5 +6,5 @@ public class ApiService {
     public static void   connectToPortalApi(String portal, String token) {
         // TODO: Implement actual connection to portal's API
         System.out.println("Connecting to API of portal: " + portal);
-    } 
+    }
 }
